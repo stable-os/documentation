20496c88bb224a2490387f326a2902764f75a536
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-03-11 22:03:55 +0100

    Fix docusaurus configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,12 +6,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'My Site',
-    tagline: 'Dinosaurs are cool',
+    title: 'Stable OS Docs',
+    tagline: 'The Stable OS documentation',
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-test-site.com',
+    url: 'https://stable-os.github.io/documentation',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/documentation',
